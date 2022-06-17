9ab7a4270eb5a512e7d773a74bd4135de1500502
sayalimore1 <103490934+sayalimore1@users.noreply.github.com>
2022-06-17 11:06:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -123,7 +123,7 @@ resource "aws_security_group" "Sayali-sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["182.73.28.2/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -131,7 +131,7 @@ resource "aws_security_group" "Sayali-sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["182.73.28.2/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
 
